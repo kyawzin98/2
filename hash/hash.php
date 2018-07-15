2db0a8d59ed1f48ab362763c44bfff2cc8df268c
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-15 15:33:26 +0630

    finish 25-PHP password verification

--- hash/hash.php
+++ hash/hash.php
@@ -1,2 +1,8 @@
 <?php
-echo password_hash('Testing',PASSWORD_DEFAULT);
+$password= password_hash('Testing',PASSWORD_DEFAULT);
+
+echo nl2br("{$password}\n");
+$matches=password_verify('foo',$password);
+echo nl2br("{$matches}\n");
+$matches=password_verify('Testing',$password);
+echo nl2br("{$matches}\n");
