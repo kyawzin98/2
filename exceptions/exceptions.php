faa3e166eef7950de6213ff8e6d4293f041af8bd
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-15 23:51:35 +0630

    finish 37-Extend PHP exceptions

--- exceptions/exceptions.php
+++ exceptions/exceptions.php
@@ -1,19 +1,26 @@
 <?php
+class InvalidCCNumberException extends InvalidArgumentException{
+    public function __construct($message = "No CC Number", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
 try{
     processCC();
-}catch (Exception $e){
+}catch (InvalidCCNumberException $e){
     echo $e->getMessage().' ';
     echo get_class($e);
     echo nl2br("\n");
-    echo $e->getPrevious()->getMessage().' ';
-    echo get_class($e->getPrevious());
+//    echo $e->getPrevious()->getMessage().' ';
+//    echo get_class($e->getPrevious());
 
 }
 function processCC($numb=null,$zipCode=null){
     try{
         validate($numb,$zipCode);
     }catch (Exception $e){
-        throw new BadFunctionCallException('Invalid Input',null,$e);
+//        throw new BadFunctionCallException('Invalid Input',null,$e);
+        throw $e;
     }
     echo "processed";
 }
@@ -21,6 +28,6 @@ function processCC($numb=null,$zipCode=null){
 
 function validate($numb,$zipCode){
     if (is_null($numb)){
-        throw new InvalidArgumentException('No CC Number');
+        throw new InvalidCCNumberException();
     }
 }
