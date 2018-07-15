f7cb2b7905a67ae41121322f412a3532bb637109
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-15 23:21:50 +0630

    finish 35-Nested exceptions

--- exceptions/exceptions.php
+++ exceptions/exceptions.php
@@ -3,10 +3,21 @@ try{
     processCC();
 }catch (Exception $e){
     echo $e->getMessage();
+    echo nl2br("\n");
+    echo $e->getPrevious()->getMessage();
 }
 function processCC($numb=null,$zipCode=null){
+    try{
+        validate($numb,$zipCode);
+    }catch (Exception $e){
+        throw new Exception('Invalid Input',null,$e);
+    }
+    echo "processed";
+}
+
+
+function validate($numb,$zipCode){
     if (is_null($numb)){
         throw new Exception('No CC Number');
     }
-    echo "processed";
 }
