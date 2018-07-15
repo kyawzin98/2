dde6649134c43f3848995535946fe37744eb23a0
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-15 07:45:46 +0630

    finish 09-Extend an interface

--- interfaces/interface.php
+++ interfaces/interface.php
@@ -1,13 +1,21 @@
 <?php
 interface TableInterface{
     public function save(array $data);
+}interface LogInterface{
+    public function log($message);
 }
-class Table implements TableInterface{
+class Table implements TableInterface,LogInterface {
     public function save(array $data)
     {
         // TODO: Implement save() method.
         return 'foo';
     }
+    public function log($message)
+    {
+        // TODO: Implement log() method.
+        return $message . "\n";
+    }
 }
 
 echo (new Table())->save([]);
+echo (new Table())->log(' interfaces are awesome');
