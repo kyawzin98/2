4865c10879d66584d4582345f852771452c3d0da
Kyaw Zin <immortalityskill2398@gmail.com>
2018-07-15 15:42:57 +0630

    finish 26-Update PHP passwords

--- hash/hash.php
+++ hash/hash.php
@@ -2,7 +2,14 @@
 $password= password_hash('Testing',PASSWORD_DEFAULT);
 
 echo nl2br("{$password}\n");
-$matches=password_verify('foo',$password);
-echo nl2br("{$matches}\n");
-$matches=password_verify('Testing',$password);
-echo nl2br("{$matches}\n");
+//$matches=password_verify('foo',$password);
+//echo nl2br("{$matches}\n");
+//$matches=password_verify('Testing',$password);
+//echo nl2br("{$matches}\n");
+
+if (password_needs_rehash($password,PASSWORD_DEFAULT,['cost'=>12])){
+    $newHash=password_hash($password,PASSWORD_DEFAULT,['cost'=>12]);
+
+    echo nl2br("{$newHash}\n");
+
+}
